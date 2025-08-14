49e0960e7bb1651476193be9c3dcc87817e2d71e
GrayKOT88 <skotkin@yandex.ru>
2025-08-14 17:54:47 +0300

    14.08.25

--- My first RPG/Assets/Scripts/Inventory & Shop/ItemSO.cs
+++ My first RPG/Assets/Scripts/Inventory & Shop/ItemSO.cs
@@ -6,4 +6,15 @@ public class ItemSO : ScriptableObject
     public string itemName;
     [TextArea] public string itemDescription;
     public Sprite icon;
+
+    public bool isGold;
+
+    [Header("Stats")]
+    public int currentHealth;
+    public int maxHealth;
+    public int speed;
+    public int damage;
+
+    [Header("For Temporary Items")]
+    public float duration;
 }
