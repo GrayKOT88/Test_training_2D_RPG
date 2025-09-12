5328cde049dc1b92939b9c1f49fa5fa1d96b5600
GrayKOT88 <skotkin@yandex.ru>
2025-09-12 17:07:08 +0300

    12.09.25

---
+++ My first RPG/Assets/Scripts/QuestSystem/QuestLogUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class QuestLogUI : MonoBehaviour
+{
+    
+    public void HandleQuestClicked(QuestSO questSO)
+    {
+        Debug.Log($"Clicked Quest: {questSO.questName}");
+        foreach (var objective  in questSO.objectives)
+        {
+            Debug.Log($"Objective: {objective.description}");
+        }
+    }
+}
